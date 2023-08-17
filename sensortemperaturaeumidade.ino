8f0a9a96960fe71a660b492f745d89584750deeb
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-08-17 09:44:45 -0300

    retirando comentarios

--- sensortemperaturaeumidade.ino
+++ sensortemperaturaeumidade.ino
@@ -1,25 +1,18 @@
-#include <DHT.h> // Biblioteca DHT
-
+#include <DHT.h> 
 #define DHT_PIN 2 // Pino de comunicação do sensor DHT11
-
-DHT dht(DHT_PIN, DHT11); // Inicializa o objeto DHT
-
+DHT dht(DHT_PIN, DHT11); 
 void setup() {
-  Serial.begin(9600); // Inicializa a comunicação serial
-  dht.begin(); // Inicializa o sensor DHT11
+  Serial.begin(9600);
+  dht.begin();
 }
-
 void loop() {
-  float temperatura = dht.readTemperature(); // Lê a temperatura em graus Celsius
-  float umidade = dht.readHumidity(); // Lê a umidade relativa do ar
-
+  float temperatura = dht.readTemperature();
+  float umidade = dht.readHumidity();
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
   Serial.println("°C");
-
   Serial.print("Umidade: ");
   Serial.print(umidade);
   Serial.println("%");
-
-  delay(2000); // Aguarda 2 segundos antes de ler novamente
+  delay(2000);
 }
